cb53232755280953a351f0198f8cf895a7d08b19
Bjorn-Vegar Borge <bvborge@gmail.com>
2025-05-13 12:18:39 +0200

    Added a simple bash copy script

---
+++ copy_default_vars.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# Function to copy file if it does not exist
+copy_if_not_exists() {
+    local source=$1
+    local destination=$2
+
+    if [ ! -f "$destination" ]; then
+        cp "$source" "$destination"
+        echo "Copied '$source' to '$destination'."
+    else
+        echo "File '$destination' already exists. Skipping copy."
+    fi
+}
+
+# Copy default variables from role 'zia_url_filtering_rules' to host variables for localhost
+copy_if_not_exists ./roles/zscaler_zia/zia_url_filtering_rules/defaults/main.yaml ./inventory/host_vars/localhost/zia_url_filtering_rules.yaml
+
+# Copy default variables from role 'zia_ssl_inspection_rules' to host variables for localhost
+copy_if_not_exists ./roles/zscaler_zia/zia_ssl_inspection_rules/defaults/main.yaml ./inventory/host_vars/localhost/zia_ssl_inspection_rules.yaml
