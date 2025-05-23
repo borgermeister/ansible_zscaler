ee29cad09997dc109ce00f6425cdaf8092426302
Bjorn-Vegar Borge <bvborge@gmail.com>
2025-05-23 12:39:01 +0200

    Added firewall rule to copy script

--- copy_default_vars.sh
+++ copy_default_vars.sh
@@ -13,8 +13,12 @@ copy_if_not_exists() {
     fi
 }
 
-# Copy default variables from role 'zia_url_filtering_rules' to host variables for localhost
-copy_if_not_exists ./roles/zscaler_zia/zia_url_filtering_rules/defaults/main.yaml ./inventory/host_vars/localhost/zia_url_filtering_rules.yaml
+
+# Copy default variables from role 'zia_cloud_firewall_rule' to host variables for localhost
+copy_if_not_exists ./roles/zscaler_zia/zia_cloud_firewall_rule/defaults/main.yaml ./inventory/host_vars/localhost/zia_cloud_firewall_rule.yaml
 
 # Copy default variables from role 'zia_ssl_inspection_rules' to host variables for localhost
 copy_if_not_exists ./roles/zscaler_zia/zia_ssl_inspection_rules/defaults/main.yaml ./inventory/host_vars/localhost/zia_ssl_inspection_rules.yaml
+
+# Copy default variables from role 'zia_url_filtering_rules' to host variables for localhost
+copy_if_not_exists ./roles/zscaler_zia/zia_url_filtering_rules/defaults/main.yaml ./inventory/host_vars/localhost/zia_url_filtering_rules.yaml
